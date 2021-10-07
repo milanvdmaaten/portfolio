8088e63a23247ffc5342aaba278082ca8b55d141
xiduzo <mail@sanderboer.nl>
2021-10-07 23:13:13 +0200

    Edited some name configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Sander Boer`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sander Boer`,
+      summary: `developer, designer, tinkerer.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Portfolio website Sander Boer.`,
+    siteUrl: `https://xiduzo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      instagram: `xiduz0`,
+      twitter: `xiduzo`,
     },
   },
   plugins: [
@@ -112,13 +113,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Portfolio Sander Boer`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
