a19fcb717a80de076c6ae6c9fe225d32bab05723
sander boer <mail@sanderboer.nl>
2021-10-11 21:52:32 +0200

    added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,5 +130,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`, // https://www.gatsbyjs.com/plugins/gatsby-plugin-netlify/
   ],
 }
