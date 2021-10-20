0ec4dfea68fdace003964511153f60fb126f2474
sander boer <mail@sanderboer.nl>
2021-10-21 01:58:05 +0200

    art 2

--- src/components/backgroundDrawer.js
+++ src/components/backgroundDrawer.js
@@ -5,88 +5,109 @@ export const BackgroundDrawer = () => {
 
   // Create the canvas
   React.useEffect(() => {
-    setTimeout(() => {
-      const body = document.body
-      const html = document.documentElement
-      let isDrawing = false
-      const prev = { x: undefined, y: undefined }
+    const body = document.body
+    const html = document.documentElement
+    let isDrawing = false
+    let currentPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    )
 
-      const height = Math.max(
-        body.scrollHeight,
-        body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
+    const height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    )
+
+    const width = Math.max(
+      body.scrollWidth,
+      body.offsetWidth,
+      html.clientWidth,
+      html.scrollWidth,
+      html.offsetWidth
+    )
+
+    const main = document.getElementById("layout")
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+
+    svg.setAttribute("width", width)
+    svg.setAttribute("height", height)
+    svg.setAttribute("viewBox", `0,0,${width}, ${height}`)
+
+    svg.style.zIndex = -1
+    svg.style.height = height + "px"
+    svg.style.position = "absolute"
+    svg.style.width = "100%"
+    svg.style.top = 0
+
+    const drawLine = (from, to) => {
+      const line = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "line"
       )
+      line.setAttribute("id", "line")
+      line.setAttribute("x1", from.x)
+      line.setAttribute("x2", to.x)
+      line.setAttribute("y1", from.y)
+      line.setAttribute("y2", to.y)
+      line.setAttribute("stroke", "black")
 
-      const width = Math.max(html.clientWidth)
+      svg.appendChild(line)
+    }
 
-      const main = document.getElementById("layout")
-      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    drawLine({ x: 0, y: 0 }, { x: width, y: height })
 
-      svg.setAttribute("width", width)
-      svg.setAttribute("height", height)
-      svg.setAttribute("viewBox", `0,0,${width}, ${height}`)
+    main.appendChild(svg)
 
-      svg.style.zIndex = -1
-      svg.style.height = height + "px"
-      svg.style.position = "absolute"
-      svg.style.width = "100%"
-      svg.style.top = 0
+    const onresize = () => {
+      svg.setAttribute(
+        "viewBox",
+        `0,0,${window.innerWidth}, ${window.innerHeight}`
+      )
+    }
 
-      const drawLine = (from, to) => {
-        const line = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "line"
-        )
-        line.setAttribute("id", "line")
-        line.setAttribute("x1", from.x)
-        line.setAttribute("x2", to.x)
-        line.setAttribute("y1", from.y)
-        line.setAttribute("y2", to.y)
-        line.setAttribute("stroke", "black")
+    const onMouseMove = event => {
+      const { pageX, pageY } = event
 
-        svg.appendChild(line)
-      }
+      if (!isDrawing) return
 
-      drawLine({ x: 0, y: 0 }, { x: width, y: height })
+      const line = currentPath.getAttribute("d")
+      if (!line) currentPath.setAttribute("d", `M${pageX},${pageY}`)
+      console.log(currentPath, currentPath.getAttribute("d"))
 
-      main.appendChild(svg)
+      currentPath.setAttribute(
+        "d",
+        `${currentPath.getAttribute("d")} L${pageX}, ${pageY}`
+      )
+    }
 
-      const onresize = () => {
-        svg.setAttribute(
-          "viewBox",
-          `0,0,${window.innerWidth}, ${window.innerHeight}`
-        )
-      }
+    const onMouseDown = () => {
+      console.log("mouseDown")
+      isDrawing = true
+      currentPath = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      )
+      currentPath.setAttribute("stroke", "black")
 
-      const onMouseMove = event => {
-        const { pageX, pageY } = event
-        console.log(pageX, pageY)
-        // if (!isDrawing) return
-        if (!prev.x || !prev.y) {
-          prev.x = pageX
-          prev.y = pageY
-        }
+      svg.appendChild(currentPath)
+    }
 
-        drawLine({ x: prev.x, y: prev.y }, { x: pageX, y: pageY })
-      }
+    const onMouseUp = () => {
+      isDrawing = false
+      console.log("mouseUp")
+    }
 
-      const onMouseDown = () => {
-        console.log("mouseDown")
-        isDrawing = true
-      }
+    window.onresize = onresize
+    window.onmousemove = onMouseMove
+    window.onmousedown = onMouseDown
+    window.onmouseup = onMouseUp
 
-      const onMouseUp = () => {
-        isDrawing = false
-        console.log("mouseUp")
-      }
-
-      window.onresize = onresize
-      window.onmousemove = onMouseMove
-      window.onmousedown = onMouseDown
-      window.onmouseup = onMouseUp
-    }, 1000 * 5)
+    return () => {
+      svg.remove()
+    }
   }, [])
 
   return null
